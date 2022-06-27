6daa3f2209d78ac0757780e17512916924f06313
sam2881 <48903787+sam2881@users.noreply.github.com>
2022-06-27 20:12:28 +0530

    Update RecommendationModel.py

--- RecommendationModel.py
+++ RecommendationModel.py
@@ -1,6 +1,6 @@
-import pickle
-from flask import jsonify
+from flask import jsonify, Flask
 import numpy as np
+import pickle
 
 
 
@@ -9,19 +9,17 @@ class RecommendationModel(object):
     Model template. You can load your model parameters in __init__ from a location accessible at runtime
     """
 
-def __init__(self):
-    # Add any initialization parameters and model data that are loaded.
-    print("Initializing")
+    def __init__(self):
+        # Add any initialization parameters and model data that are loaded.
+        print("Initializing")
 
-    self.trainedModel = pickle.load(open('Model/' + "model_gb_ver0.pickle", 'rb'))
+        self.trainedModel = pickle.load(open('Model/' + "model_gb_ver0.pickle", 'rb'))
 
-    # self.model_trained = joblib.load('Model/' + 'SVM_Gov_Measures_model.sav')
-
-
-def predict(self, request):    
-   
-    y = request['data']['ndarray'][0]
-    prediction = self.trainedModel.predict(y)
-    return prediction
+        # self.model_trained = joblib.load('Model/' + 'SVM_Gov_Measures_model.sav')
 
 
+    def predict_raw(self,X): 
+        X = np.array(X, dtype=object)
+        y = X['data']['ndarray'][0]
+        prediction = self.trainedModel.predict(y)
+        return prediction
